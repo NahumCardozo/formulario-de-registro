ae63d907828bbd8a1a86454942b7843d211ce363
NahumCardozo <nahumcardozo14@gmail.com>
2026-09-13 13:57:52 -0300

    modificaciones leves

--- script.js
+++ script.js
@@ -2,54 +2,52 @@ const nombreUsuario = document.getElementById("nombre-y-apellido");
 const edadUsuario = document.getElementById("edad");
 const emailUsuario = document.getElementById("email");
 const contraseniaUsuario = document.getElementById("contrasenia");
-const confirmacionContraseniaUsuario = document.getElementById("confirmacion-contrasenia");
+const confirmarcontraseniaUsuario = document.getElementById("confirmacion-contrasenia");
 
+const registro = async (event) => { 
+    event.preventDefault();
 
-const registro = async (event) => {
-    event.preventDefault(); 
-
-    const nombre = nombreUsuario.value;
+    const nombreyapellido = nombreUsuario.value;
     const edad = Number(edadUsuario.value);
     const email = emailUsuario.value;
     const contrasenia = contraseniaUsuario.value;
-    const confirmacionContrasenia = confirmacionContraseniaUsuario.value;
+    const confirmarContrasenia = confirmarcontraseniaUsuario.value;
 
     const url = "https://jsonplaceholder.typicode.com/users";
     const respuesta = await fetch(url);
-    const users = await respuesta.json();
+    const user = await respuesta.json();
 
-    const usuariosRegistrados = users.map((usuario) => usuario.email);
-    const emailEncontrado = usuariosRegistrados.filter((emailUser) => emailUser === email);
+    const usuariosRegistrados = user.map((usuario) => usuario.email);
+    const emailIngresado = usuariosRegistrados.filter ((emailUser) => emailUser === email)
 
-    if (emailEncontrado.length > 0) {
-        alert("El email ingresado ya está registrado. Intenta con otro.");
+    if (emailIngresado.length > 0) {
+        alert("El email ingresado ya se encuentra registrado, por favor ingrese otro email.");
         return;
     }
 
-    if (contrasenia !== confirmacionContrasenia) {
+    if (contrasenia !== confirmarContrasenia) {
         alert("Las contraseñas no coinciden.");
         return;
     }
-
+    
     const edadAprobada = 18;
     if (edad < edadAprobada) {
-        alert("Necesitas ser mayor de edad para registrarte.");
+        alert("Debes ser mayor de edad para registrarte.");
         return;
     }
 
-    if (edad === edadAprobada) {
-        alert("Puedes ingresar pero con supervisión.");
+    if (edad >= edadAprobada) {
+        alert("Registro exitoso.");
     }
 
     localStorage.setItem("emailUsuario", email);
     window.location.href = "bienvenida.html";
 };
 
-
-const usuarioLogueado = () => {
+const usuariologeado = () => {
     const emailActivo = localStorage.getItem("emailUsuario");
     const ruta = window.location.pathname;
-    
+
     if (emailActivo && (ruta.endsWith("index.html") || ruta === "/")) {
         window.location.href = "bienvenida.html";
     }
@@ -59,10 +57,12 @@ const usuarioLogueado = () => {
     }
 };
 
-usuarioLogueado();
-
+usuariologeado();
 
 const logout = () => {
     localStorage.clear();
     window.location.href = "index.html";
-};
+}; 
+
+
+
